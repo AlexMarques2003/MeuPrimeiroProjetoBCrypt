ad1cff73f06cf1460e7273c4ef993d8d07417abc
Antonio Alex Marques Albuquerque <alex.marques@textiluniao.com.br>
2018-08-02 14:34:08 -0300

    Primeiro commit

---
+++ src/main/java/com/alexmarques2003/api/MeuPrimeiroProjetoBcryptApplication.java
@@ -0,0 +1,25 @@
+package com.alexmarques2003.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.alexmarques2003.utils.SenhaUtils;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoBcryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoBcryptApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded:" + senhaEncoded);
+			System.out.println("Senha válida:" + SenhaUtils.senhaValida("123456",senhaEncoded));
+		};
+	}
+}
---
+++ src/main/java/com/alexmarques2003/utils/SenhaUtils.java
@@ -0,0 +1,35 @@
+package com.alexmarques2003.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SenhaUtils {
+	
+	/*
+	 * Gera um hash utilizando o BCrypt
+	 * 
+	 * @param senha
+	 * @return String
+	 * */
+	
+	public static String gerarBCrypt(String senha) {
+		if(senha==null) {
+			return senha;
+		}
+		
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(senha);
+	}
+	
+	/* Verifica se a senha é válida
+	 * 
+	 * @param senha
+	 * @param senhaEncoded
+	 * @return boolean
+	 * */
+	public static boolean senhaValida(String senha, String senhaEnconded) {
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.matches(senha, senhaEnconded);
+		
+	}
+	
+}
